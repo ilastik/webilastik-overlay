2840bdd1c1caf6680fa320e7c2d7423fbc0d5465
Tomaz Vieira <tom.fogaca@gmail.com>
2020-06-16 18:29:20 +0200

    Adds ortho camera

--- src/camera.ts
+++ src/camera.ts
@@ -1,16 +1,28 @@
 import {vec3, mat4} from "gl-matrix"
 
-export interface Camera{
-    world_to_view_matrix: mat4
-    view_to_device_matrix: mat4
-}
 
-export class PerspectiveCamera implements Camera{
+export abstract class Camera{
     position = vec3.fromValues(0,0,0)
 
     world_to_view_matrix = mat4.create()
     view_to_device_matrix = mat4.create()
 
+
+    public moveTo(position: vec3){
+        vec3.copy(this.position, position)
+    }
+
+    public lookAt(point: vec3, up=vec3.fromValues(0,1,0)): void{
+        mat4.lookAt(
+            /*out=*/this.world_to_view_matrix,
+            /*eye=*/this.position,
+            /*center=*/point,
+            /*up=*/up
+        )
+    }
+}
+
+export class PerspectiveCamera extends Camera{
     fovy: number
     aspect: number
     near: number
@@ -21,19 +33,18 @@ export class PerspectiveCamera implements Camera{
         near?: number,
         far?: number
     }){
+        super()
         mat4.perspective(this.view_to_device_matrix, fovy, aspect, near, far)
     }
+}
 
-    public moveTo(position: vec3){
-        vec3.copy(this.position, position)
-    }
-
-    public lookAt(point: vec3, up=vec3.fromValues(0,1,0)){
-        mat4.lookAt(
-            /*out=*/this.world_to_view_matrix,
-            /*eye=*/this.position,
-            /*center=*/point,
-            /*up=*/up
-        )
+export class OrthoCamera extends Camera{
+    constructor({left, right, bottom, top, near, far}: {
+        left: number, right: number,
+        bottom: number, top: number,
+        near: number, far: number,
+    }){
+        super()
+        mat4.ortho(this.view_to_device_matrix, left, right, bottom, top, near, far)
     }
 }
