bb37fd6536bed907cb9e22c5ed83fe65590e79a3
Tomaz Vieira <tom.fogaca@gmail.com>
2021-02-02 14:33:29 +0100

    Makes background of target div black in main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ let dummy_viewer = createElement({tagName: "div", parentElement: document.body, 
     width: "400px",
     height: "300px",
     border: "solid 5px purple",
+    backgroundColor: "black",
 }})
 
 let voxelToWorld = mat4.fromScaling(mat4.create(), vec3.fromValues(1, 1, 1))
