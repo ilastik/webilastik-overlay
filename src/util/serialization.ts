4bf53765f026955425ce0bfbb30c820edebad409
Tomaz Vieira <tom.fogaca@gmail.com>
2021-05-10 14:54:43 +0200

    Simplifies serialization of json values

--- src/util/serialization.ts
+++ src/util/serialization.ts
@@ -6,16 +6,18 @@ export interface IJsonableObject{
     [key: string]: Jsonable
 }
 
-export type Jsonable = number | string | null | IJsonable | Array<Jsonable> | IJsonableObject
+export type JsonLeaf = number | string | boolean | null
+
+export type Jsonable = JsonLeaf | IJsonable | Array<Jsonable> | IJsonableObject
 
 export function toJsonValue(value: Jsonable) : Jsonable{
-    if(typeof value == "number" || typeof value == "string" || value === null){
+    if(isLeaf(value)){
         return value
     }
-    if(value instanceof Array){
+    if(isArray(value)){
         return value.map(val => toJsonValue(val))
     }
-    if(!("toJsonValue" in value)){
+    if(isObject(value)){
         let out : IJsonableObject = {}
         for(let key in value){
             out[key] = toJsonValue(value[key])
@@ -27,4 +29,20 @@ export function toJsonValue(value: Jsonable) : Jsonable{
 
 export interface IDeserializer<STATE extends Jsonable>{
     (data: any): STATE;
+}
+
+export function isLeaf(value: Jsonable): value is JsonLeaf{
+    return typeof value == "number" || typeof value == "string" || value === null
+}
+
+export function isArray(value: Jsonable): value is Array<Jsonable>{
+    return value instanceof Array
+}
+
+export function isIJsonable(value: Jsonable): value is IJsonable{
+    return value !== null && typeof(value) == "object" && "toJsonValue" in value
+}
+
+export function isObject(value: Jsonable): value is IJsonableObject{
+    return typeof(value) == "object" && value != null && !isIJsonable(value)
 }
