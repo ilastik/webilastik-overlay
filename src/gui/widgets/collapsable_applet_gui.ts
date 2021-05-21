cb45fa0f62949692728e699a66e10c4f710eb263
Tomaz Vieira <tom.fogaca@gmail.com>
2021-05-21 17:45:32 +0200

    Cleans up Collapsable Widget

--- src/gui/widgets/collapsable_applet_gui.ts
+++ src/gui/widgets/collapsable_applet_gui.ts
@@ -2,31 +2,40 @@ import { createElement, createInput } from "../../util/misc";
 
 export class CollapsableWidget{
     public readonly element: HTMLElement;
+    public readonly header: HTMLElement;
+    public readonly collapse_button: HTMLInputElement;
+    private _is_collapsed: boolean = false
     public constructor({display_name, parentElement}:{display_name: string, parentElement: HTMLElement}){
         this.element = createElement({tagName: "div", parentElement, cssClasses: ["CollapsableApplet"]})
-        const header = createElement({tagName: "h2", parentElement: this.element, innerHTML: display_name})
-        const collapse_button = createInput({
+        this.header = createElement({tagName: "h2", parentElement: this.element, innerHTML: display_name})
+        this.collapse_button = createInput({
             inputType: "button",
-            parentElement: header,
+            parentElement: this.header,
             value: "-",
             inlineCss: {float: "right"},
-            onClick: () => {
-                const collapsing = collapse_button.value == "-"
-                collapse_button.value = collapsing ? "□" : "-"
-                this.element.querySelectorAll("*").forEach(element => {
-                    if(!(element instanceof HTMLElement)){
-                        console.error("Found bad element in applet:")
-                        console.error(element)
-                        return
-                    }
-                    if(element != header && element != collapse_button){
-                        if(collapsing){
-                            element.classList.add("itk_hidden_element")
-                        }else{
-                            element.classList.remove("itk_hidden_element")
-                        }
-                    }
-                })
+            onClick: () => this.set_collapsed(!this._is_collapsed)
+        })
+    }
+
+    public get is_collapsed(): boolean{
+        return this._is_collapsed
+    }
+
+    public set_collapsed(collapse: boolean){
+        this._is_collapsed = collapse
+        this.collapse_button.value = collapse ? "□" : "-"
+        this.element.querySelectorAll("*").forEach(element => {
+            if(!(element instanceof HTMLElement)){
+                console.error("Found bad element in applet:")
+                console.error(element)
+                return
+            }
+            if(element != this.header && element != this.collapse_button){
+                if(collapse){
+                    element.classList.add("itk_hidden_element")
+                }else{
+                    element.classList.remove("itk_hidden_element")
+                }
             }
         })
     }
