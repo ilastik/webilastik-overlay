5e7bbe8bffa7e420c713a8cddb6fb28aa4217327
Tomaz Vieira <tom.fogaca@gmail.com>
2021-05-28 11:26:44 +0200

    Adds PopupWidget and SelectorWidget

---
+++ src/gui/widgets/selector_widget.ts
@@ -0,0 +1,71 @@
+import { createElement, createInput, removeElement } from "../../util/misc";
+import { PopupWidget } from "./popup";
+
+export class SelectorWidget<T>{
+    public readonly element: HTMLElement;
+    private selection?: T
+    constructor({title, options, optionRenderer, onSelection, parentElement}: {
+        title: string,
+        options: Array<T>,
+        optionRenderer: (option: T) => string,
+        onSelection: (selection?: T) => void,
+        parentElement?: HTMLElement,
+    }){
+        let popup: PopupWidget | undefined;
+        if(parentElement === undefined){
+            popup = new PopupWidget(title)
+            this.element = popup.element
+        }else{
+            popup = undefined
+            this.element = createElement({tagName: "div", parentElement})
+        }
+        this.element.classList.add("ItkSelector")
+
+        let form = createElement({tagName: "form", parentElement: this.element})
+        options.forEach(opt => {
+            const p = createElement({tagName: "p", parentElement: form})
+            createInput({inputType: "radio", name: "option_selection", parentElement: p, onClick: () => {
+                this.selection = opt
+            }})
+
+            const display_str = optionRenderer(opt)
+            createElement({tagName: "label", parentElement: p, innerHTML: " " + display_str})
+        })
+
+        let p = createElement({tagName: "p", parentElement: form})
+        createInput({inputType: "submit", value: "Ok", parentElement: p})
+        createInput({inputType: "button", value: "Cancel", parentElement: p, onClick: () => {
+            close(undefined)
+        }})
+
+        const close = (selection?: T) => {
+            popup?.destroy()
+            removeElement(this.element)
+            onSelection(selection)
+        }
+
+        form.addEventListener("submit", (ev) => {
+            close(this.selection)
+            //don't submit synchronously
+            ev.preventDefault()
+            return false
+        })
+    }
+
+    public static select<T>({title, options, optionRenderer, parentElement}: {
+        title: string,
+        options: Array<T>,
+        optionRenderer: (option: T) => string,
+        parentElement?: HTMLElement,
+    }): Promise<T | undefined>{
+        return new Promise(resolve => {
+            new SelectorWidget({
+                title,
+                options,
+                optionRenderer,
+                parentElement,
+                onSelection: (opt?: T) => resolve(opt)
+            })
+        })
+    }
+}
