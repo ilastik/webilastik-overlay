1a46652234d79220a83ffa25f11cfac78d73e4ea
Tomaz Vieira <tom.fogaca@gmail.com>
2021-06-14 15:19:27 +0200

    Moves brush stroke widgets container into its own class

--- src/gui/widgets/brushing_overlay/brushing_widget.ts
+++ src/gui/widgets/brushing_overlay/brushing_widget.ts
@@ -1,11 +1,9 @@
 import { quat, vec3 } from "gl-matrix"
 import { IViewerDriver, BrushStroke } from "../../.."
-import { Applet } from "../../../client/applets/applet"
 import { DataSource, Session } from "../../../client/ilastik"
 import { IDataSourceScale, IViewportDriver } from "../../../drivers/viewer_driver"
-import { createElement, vec3ToRgb, vecToString, createSelect, createInput, removeElement } from "../../../util/misc"
+import { createElement, createSelect, createInput, removeElement } from "../../../util/misc"
 import { PrecomputedChunks } from "../../../util/precomputed_chunks_datasource"
-import { ensureJsonArray } from "../../../util/serialization"
 import { CollapsableWidget } from "../collapsable_applet_gui"
 import { SimpleSelectorWidget } from "../selector_widget"
 import { Vec3ColorPicker } from "../vec3_color_picker"
@@ -13,20 +11,22 @@ import { BrushingOverlay } from "./brushing_overlay"
 import { BrushelBoxRenderer } from "./brush_boxes_renderer"
 import { BrushelLinesRenderer } from "./brush_lines_renderer"
 import { BrushRenderer } from "./brush_renderer"
+import { BrushStrokesContainer } from "./brush_strokes_container"
 
-export class BrushingWidget extends Applet<Array<BrushStroke>>{
+export class BrushingWidget{
     public readonly viewer_driver: IViewerDriver
     public readonly element: HTMLElement
-    private readonly brushStrokesContainer: HTMLElement
 
     public readonly colorPicker: Vec3ColorPicker
     public readonly rendererDropdown: RendererDropdown
     private readonly overlay: BrushingOverlay
-    private brushStrokeWidgets: Array<BrushStrokeWidget> = []
+    private brushStrokeContainer: BrushStrokesContainer
     private brushing_scale_selector: SimpleSelectorWidget<IDataSourceScale>
     public readonly status_display: HTMLElement
     private refreshStart: Date = new Date()
     public readonly brushing_enabled_checkbox: HTMLInputElement
+
+    private staging_brush_stroke: BrushStroke | undefined = undefined
 
     constructor({
         session,
@@ -37,30 +37,23 @@ export class BrushingWidget extends Applet<Array<BrushStroke>>{
         parentElement: HTMLElement,
         viewer_driver: IViewerDriver,
     }){
-        super({
-            name: "brushing_applet",
-            deserializer: (data) => {
-                let raw_annotations = ensureJsonArray(data);
-                return raw_annotations.map(a => BrushStroke.fromJsonValue(this.overlay.gl, a))
-            },
-            session,
-            onNewState: (new_state) => this.onNewState(new_state)
-        })
         this.overlay = new BrushingOverlay({
             viewer_driver,
             brush_stroke_handler: {
                 handleNewBrushStroke: (params: {start_position_uvw: vec3, camera_orientation_uvw: quat}) => {
-                    const stroke = new BrushStroke({
+                    this.staging_brush_stroke = new BrushStroke({
                         gl: this.overlay.gl,
                         start_postition: params.start_position_uvw, //FIXME put scale somewhere
                         color: this.colorPicker.getColor(),
                         annotated_data_source: new DataSource(this.brushing_scale_selector.getSelection()!.url),
                         camera_orientation: params.camera_orientation_uvw, //FIXME: realy data space? rename param in BrushStroke?
                     })
-                    this.doAddBrushStroke(stroke)
-                    return stroke
+                    return this.staging_brush_stroke
                 },
-                handleFinishedBrushStroke: (_) => this.updateUpstreamState(this.getBrushStrokes())
+                handleFinishedBrushStroke: (stroke) => {
+                    this.brushStrokeContainer.addBrushStroke(stroke)
+                    this.staging_brush_stroke = undefined
+                }
             },
         })
         this.element = new CollapsableWidget({display_name: "Training", parentElement}).element
@@ -105,13 +98,20 @@ export class BrushingWidget extends Applet<Array<BrushStroke>>{
                 ])
             })
 
-        createElement({tagName: "h2", innerHTML: "Brush Strokes", parentElement: this.element})
-        this.brushStrokesContainer = createElement({tagName: "table", parentElement: this.element, cssClasses: ["ItkBrushingWidget_strokes-container"]})
+        p = createElement({tagName: "p", parentElement: this.element})
+        createElement({tagName: "label", innerHTML: "Brush Strokes:", parentElement: p})
+        this.brushStrokeContainer = new BrushStrokesContainer({
+            parentElement: this.element, session, applet_name: "brushing_applet", gl: this.overlay.gl, onBrushColorClicked: (color: vec3) => this.colorPicker.setColor(color)
+        })
 
         this.refreshDatasource()
 
         let render = () => {
-            this.overlay.render(this.brushStrokeWidgets.map(w => w.brushStroke), this.rendererDropdown.getRenderer())
+            let strokes = this.brushStrokeContainer.getBrushStrokes()
+            if(this.staging_brush_stroke){
+                strokes.push(this.staging_brush_stroke)
+            }
+            this.overlay.render(strokes, this.rendererDropdown.getRenderer())
             window.requestAnimationFrame(render)
         }
 
@@ -179,97 +179,10 @@ export class BrushingWidget extends Applet<Array<BrushStroke>>{
         }
     }
 
-    public addBrushStroke(brushStroke: BrushStroke){
-        this.doAddBrushStroke(brushStroke)
-        this.updateUpstreamState(this.brushStrokeWidgets.map(bsw => bsw.brushStroke))
-    }
-
-    protected doAddBrushStroke(brushStroke: BrushStroke){
-        this.brushStrokeWidgets.push(
-            new BrushStrokeWidget({
-                brushStroke,
-                parentElement: this.brushStrokesContainer,
-                onColorClicked: (color: vec3) => {
-                    this.colorPicker.setColor(color);
-                },
-                onLabelClicked: (_) => {
-                    //FIXME: snap viewer to coord
-                },
-                onDeleteClicked: (stroke) => {
-                    let updated_strokes = this.getBrushStrokes().filter(stk => stk != stroke)
-                    this.onNewState(updated_strokes)
-                    this.updateUpstreamState(updated_strokes)
-                }
-            })
-        )
-    }
-
-    public getBrushStrokes(): Array<BrushStroke>{
-        return this.brushStrokeWidgets.map(bsw => bsw.brushStroke)
-    }
-
-    protected onNewState(brush_strokes: Array<BrushStroke>){
-        this.brushStrokeWidgets.forEach(bsw => bsw.destroy())
-        this.brushStrokeWidgets = []
-        brush_strokes.forEach(stroke => this.doAddBrushStroke(stroke))
-    }
-
     public destroy(){
         this.overlay.destroy()
+        this.brushStrokeContainer.destroy()
         removeElement(this.element)
-    }
-}
-
-export class BrushStrokeWidget{
-    public readonly element: HTMLElement
-    public readonly brushStroke: BrushStroke
-
-    constructor({brushStroke, parentElement, onLabelClicked, onColorClicked, onDeleteClicked}:{
-        brushStroke: BrushStroke,
-        parentElement: HTMLElement,
-        onLabelClicked : (stroke: BrushStroke) => void,
-        onColorClicked : (color: vec3) => void,
-        onDeleteClicked : (stroke: BrushStroke) => void,
-    }){
-        this.brushStroke = brushStroke
-        this.element = createElement({tagName: "tr", parentElement, cssClasses: ["BrushStrokeWidget"], inlineCss: {
-            listStyleType: "none",
-        }})
-
-        const color_container = createElement({tagName: "td", parentElement: this.element})
-        createInput({
-            inputType: "button",
-            value: "🖌",
-            parentElement: color_container,
-            inlineCss: {
-                backgroundColor: vec3ToRgb(brushStroke.color),
-            },
-            onClick: () => onColorClicked(brushStroke.color),
-        })
-
-        createElement({
-            parentElement: this.element,
-            tagName: "td",
-            innerHTML: `at voxel ${vecToString(brushStroke.getVertRef(0), 0)}`,
-            onClick: () => onLabelClicked(brushStroke),
-            inlineCss: {
-                cursor: "pointer"
-            }
-        })
-
-        const close_button_cell = createElement({parentElement: this.element, tagName: "td"})
-        createInput({
-            inputType: "button",
-            value: "✖",
-            parentElement: close_button_cell,
-            cssClasses: ["delete_brush_button"],
-            onClick: () => onDeleteClicked(brushStroke),
-        })
-    }
-
-    public destroy(){
-        this.brushStroke.destroy()
-        this.element.parentElement?.removeChild(this.element)
     }
 }
 
